14407eee082085c6fddba6905af6a900b9ce2056
Arpit Shukla <shills.arpit68@gmail.com>
2023-06-12 14:59:54 +0530

    Completed testing the project

--- main.go
+++ main.go
@@ -1,19 +1,45 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/joho/godotenv"
-	"github.com/slack-go/slack"
+	"github.com/shomali11/slacker"
 )
 
 func main() {
 	err := godotenv.Load()
-	Auth_Token := os.Getenv("AUTH_TOKEN")
+	BOT_TOKEN := os.Getenv("BOT_TOKEN")
+	APP_TOKEN := os.Getenv("SLACK_APP_TOKEN")
 	if err != nil {
 		log.Fatal("Error loading the env file")
 	}
-	api := slack.New(os.Getenv("AUTH_TOKEN"))
-	api.PostMessage()
+
+	bot := slacker.NewClient(BOT_TOKEN, APP_TOKEN)
+	bot.Command("my yob is {year}", &slacker.CommandDefinition{
+		Description: "Calculate the age",
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+			word := request.Param("year")
+			yob, err := strconv.Atoi(word)
+			if err != nil {
+				log.Fatal("Error in converting to int from string")
+			}
+			curr_year := time.Now().Year()
+			age := curr_year - yob
+			curr_age := strconv.Itoa(age)
+			response.Reply("Your age is " + curr_age)
+		},
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err = bot.Listen(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
